162589c5a5b2b8a3380666b98e144bd8684d6b04
Christoph Tack <prog.send@gmail.com>
2023-12-27 15:38:00 +0100

    Codepage update

--- firmware/esp32/platformio/tests/o-led-test/src/main.cpp
+++ firmware/esp32/platformio/tests/o-led-test/src/main.cpp
@@ -79,154 +79,22 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h)
 
 void testdrawchar(void)
 {
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextWrap(false);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
+  display.cp437(true);  // true code page 437 (without Adafruit bug, see https://learn.adafruit.com/adafruit-gfx-graphics-library/graphics-primitives
 
   for (uint8_t i = 0; i < 168; i++)
   {
     if (i == '\n')
       continue;
-    display.write(i);
+    display.write(i%2 ? '\xDB' : '\xB0');
     if ((i > 0) && (i % 21 == 0))
       display.println();
   }
   display.display();
-}
-
-void testdrawcircle(void)
-{
-  for (uint8_t i = 0; i < display.height(); i += 2)
-  {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, WHITE);
-    display.display();
-  }
-}
-
-void testfillrect(void)
-{
-  uint8_t color = 1;
-  for (uint8_t i = 0; i < display.height() / 2; i += 3)
-  {
-    // alternate colors
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, color % 2);
-    display.display();
-    color++;
-  }
-}
-
-void testdrawtriangle(void)
-{
-  for (uint16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5)
-  {
-    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    display.display();
-  }
-}
-
-void testfilltriangle(void)
-{
-  uint8_t color = WHITE;
-  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5)
-  {
-    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, WHITE);
-    if (color == WHITE)
-      color = BLACK;
-    else
-      color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawroundrect(void)
-{
-  for (uint8_t i = 0; i < display.height() / 3 - 2; i += 2)
-  {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, WHITE);
-    display.display();
-  }
-}
-
-void testfillroundrect(void)
-{
-  uint8_t color = WHITE;
-  for (uint8_t i = 0; i < display.height() / 3 - 2; i += 2)
-  {
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
-    if (color == WHITE)
-      color = BLACK;
-    else
-      color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawrect(void)
-{
-  for (uint8_t i = 0; i < display.height() / 2; i += 2)
-  {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, WHITE);
-    display.display();
-  }
-}
-
-void testdrawline()
-{
-  for (uint8_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(0, 0, i, display.height() - 1, WHITE);
-    display.display();
-  }
-  for (uint8_t i = 0; i < display.height(); i += 4)
-  {
-    display.drawLine(0, 0, display.width() - 1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (uint8_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(0, display.height() - 1, i, 0, WHITE);
-    display.display();
-  }
-  for (int8_t i = display.height() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int8_t i = display.width() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, WHITE);
-    display.display();
-  }
-  for (int8_t i = display.height() - 1; i >= 0; i -= 4)
-  {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (uint8_t i = 0; i < display.height(); i += 4)
-  {
-    display.drawLine(display.width() - 1, 0, 0, i, WHITE);
-    display.display();
-  }
-  for (uint8_t i = 0; i < display.width(); i += 4)
-  {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, WHITE);
-    display.display();
-  }
-  delay(250);
 }
 
 void setup()
@@ -240,6 +108,9 @@ void setup()
     while (1)
       yield();
   }
+  
+  testdrawchar();
+  return;
 
   for (uint8_t rot = 0; rot < 4; rot++)
   {
