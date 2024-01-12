7a5d504c5a096151c65a0425123595aaf4f9a922
Christoph Tack <prog.send@gmail.com>
2024-01-12 21:39:53 +0100

    Adapt pin numbers for PCB layout.

--- firmware/esp32/platformio/esp32-web-radio/include/pins.h
+++ firmware/esp32/platformio/esp32-web-radio/include/pins.h
@@ -6,24 +6,24 @@ typedef enum pin_def_t
 {
     // Don't use pin 12 for I2C, it's a strapping pin (MTDI), which should be pulled down to set the voltage of VDD_SDIO to 3.3V.
     // https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
-    PIN_SCL = 22,
-    PIN_SDA = 23,
+    PIN_SCL = 14,
+    PIN_SDA = 13,
     
     // I2S
-    I2S_DOUT = 25,
-    I2S_LRC = 26,
-    I2S_BCLK = 27,
+    I2S_DOUT = 23,
+    I2S_LRC = 22,
+    I2S_BCLK = 21,
 
     // Encoder 1
-    PIN_ENC1_S1 = 36,
-    PIN_ENC1_S2 = 39,
-    PIN_ENC1_KEY = 34,
+    PIN_ENC1_S1 = 33,
+    PIN_ENC1_S2 = 34,
+    PIN_ENC1_KEY = 32,
 
     // Encoder 2
-    PIN_ENC2_S1 = 33,
-    PIN_ENC2_S2 = 32,
+    PIN_ENC2_S1 = 36,
+    PIN_ENC2_S2 = 39,
     PIN_ENC2_KEY = 35,
 
     // News button
-    PIN_NEWS_BTN = 2
+    PIN_NEWS_BTN = 18
 } pin_def_t;
